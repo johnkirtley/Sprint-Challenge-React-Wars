b9da7448730ac45040eed5a6fff4e84c2c7dd24d
johnkirtley <kirtleyj16@gmail.com>
2020-01-24 15:51:18 -0800

    Accessed Films object to display Titles within character card

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -1,34 +1,48 @@
-import React, { useState, useEffect } from 'react';
-import Card from "./components/Card"
+
+import React, { useState, useEffect } from "react";
 import axios from "axios";
+import Cards from "./components/Card";
+import styled from "styled-components";
 import './App.css';
 
 const App = () => {
-  const [character, setCharacter] = useState([]);
+  const [data, setData] = useState([]);
+
 
   useEffect(() => {
     axios
       .get("https://swapi.co/api/people/")
       .then(res => {
-        setCharacter(res.data.results);
+        setData(res.data.results);
       })
       .catch(err => {
-        console.log("cannot get data", err);
+        console.log("Cannot get data", err);
       })
-  }, []);
+  }, [])
+
+  const Grid = styled.div`
+  display: grid;
+  grid-template-columns: repeat(4, 1fr);
+  `
+
+
 
 
 
   return (
     <div className="App">
       <h1 className="Header">React Wars</h1>
-      {
-        character.map((person, id) => {
-          return (
-            <Card key={id} person={person} />
-          )
-        })
-      }
+      <Grid>
+        {
+          data.map((character, id) => {
+            return (
+              <>
+                <Cards character={character} key={id} />
+              </>
+            )
+          })
+        }
+      </Grid>
     </div>
   );
 
--- starwars/src/components/Card.js
+++ starwars/src/components/Card.js
@@ -1,34 +1,63 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import styled from "styled-components";
+import Homeworld from "./Homeworld";
 
 
-const Card = (props) => {
+const Cards = (props) => {
+
+    const starshipCount = props.character.starships.length;
 
     const Container = styled.div`
-display: flex;
-flex-flow: column;
-justify-content: center;
-align-items: center;
-margin: 5% 0;
+    display: flex;
+    justify-content: center;
+    align-items:center;
+    border: 2px solid black;
+    margin: 3%;
+    background-color: lightgrey;
+
+    &:hover {
+        transform: scale(1.1);
+        transition: 0.3s ease-in;
+    }
     `
 
-    const Card = styled.div`
-    border: 2px solid black;
-    width: 45%;
-    margin:auto;
-    padding: 5% 0;
+    const CardInfo = styled.div`
+    display: flex;
+    flex-flow: column;
     `
+
+    const FilmContainer = styled.div`
+    display: flex;
+    flex-flow: column;
+    justify-content: center;
+    align-items: center;
+    border: 2px solid black;
+    
+    `
+
+
+
 
     return (
         <Container>
-            <Card>
-                <h2>{props.person.name}</h2>
-                <h3>{props.person.birth_year}</h3>
-                <p>{props.person.height}</p>
-            </Card>
+            <CardInfo>
+                <h2>Name: {props.character.name}</h2>
+                <h3>Height: {props.character.height}</h3>
+                <h3>Gender: {props.character.gender}</h3>
+                <h3>{starshipCount > 0 ? `Amount of Starships: ${starshipCount}` : 'No Starships'}</h3>
+                <h3>Films:</h3>
+                <FilmContainer>
+                    {
+                        props.character.films.map((film, id) => {
+                            return (
+                                <Homeworld film={film} key={id} />
+                            )
+                        })
+                    }
+                </FilmContainer>
+            </CardInfo>
         </Container>
-
     )
 }
 
-export default Card;
+export default Cards;
